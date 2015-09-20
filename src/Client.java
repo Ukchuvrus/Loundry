831bb820dc401fcd762659fdfaab7b0676ac2860
LeeooBloom <adicalzvch@gmail.com>
2015-09-20 12:41:07 +0300

    brrr

--- src/Client.java
+++ src/Client.java
@@ -1,25 +1,55 @@
+import java.util.Random;
+
 /**
  * Created by 111 on 17.09.2015.
  */
 
  enum  operation {
-    wash, iron, chem
+    wash, iron, chem;
 }
 
-public class Client<T> {
+public class Client{
 
-    Client(operation op){
-
+    Client(){
     }
-    private T shmot;
-    private operation op;
+    private Invoice inv;
+  public void makeInvoice() {
+      Random r = new Random();
+      operation op;
+      switch (r.nextInt(2)) {
 
-    public void give(Loundry l){
+          case 0:
+              op = operation.wash;
+              break;
+          case 1:
+              op = operation.iron;
+              break;
+          case 2:
+              op = operation.chem;
+              break;
+          default: op = operation.wash;
+      }
+          inv = new Invoice(op,r.nextInt(100),r.nextBoolean(), r.nextBoolean(),r.nextBoolean());
 
+  }
+
+    public Invoice getInv() {
+        return inv;
     }
- static public void main(String argv[]){
+
+    static public void main(String argv[]){
 
      Loundry l = new Loundry();
-     Client<Clothe> c = new Client<Clothe>(operation.wash);
+     Client c = new Client();
+     c.makeInvoice();
+        c.getInv().add(true,true,1488,true,true);
+        c.getInv().add(true,true,228,true,true);
+        c.getInv().setChem(true);
+        c.getInv().setDelicate(true);
+        c.getInv().setAuto(true);
+        c.getInv().setBleacher(false);
+        c.getInv().setOp(operation.chem);
+        c.getInv().setT(2077);
+        l.getSecretary().route(c.getInv());
  }
 }
